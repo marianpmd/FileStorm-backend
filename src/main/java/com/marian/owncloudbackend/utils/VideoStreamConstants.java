2ce44fb3506d2c50188cb47e8721430886f99eaa
Petrica Marian <marianpetrica221@gmail.com>
2022-12-19 18:49:45 +0200

    video buffer fix

--- src/main/java/com/marian/owncloudbackend/utils/VideoStreamConstants.java
+++ src/main/java/com/marian/owncloudbackend/utils/VideoStreamConstants.java
@@ -9,7 +9,7 @@ public class VideoStreamConstants {
     public static final String CONTENT_RANGE = "Content-Range";
     public static final String ACCEPT_RANGES = "Accept-Ranges";
     public static final String BYTES = "bytes";
-    public static final int CHUNK_SIZE = 8388608;
-    public static final int BYTE_RANGE = 1024;
+    public static final int CHUNK_SIZE = 9388608;
+    public static final int BYTE_RANGE = 1024 * 2;
 
 }
